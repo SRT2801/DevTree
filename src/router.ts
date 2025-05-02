b7bc9132be391af3aaf6cbf698df078112d93f48
SRT DEV <sahirruiz4@gmail.com>
2025-05-02 14:55:12 -0500

    Agregar validaciones para el registro y inicio de sesión de usuarios

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,44 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { createAccount, login } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
 
 const router = Router();
 
-router.post("/auth/register", (req, res) => {
-    console.log(req.body);
-});
+router.post(
+  "/auth/register",
+
+  body("handle")
+  .notEmpty()
+  .withMessage("El handle no puede ir vacio!"),
+
+  body("name")
+  .notEmpty()
+  .withMessage("El nombre no puede ir vacio!"),
+
+  body("email")
+  .isEmail()
+  .withMessage("Email no valido!"),
+
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("El password es muy corto, minimo 6 caracteres!"),
+  handleInputErrors,
+  createAccount
+);
+
+router.post(
+  "/auth/login",
+
+  body("email")
+  .isEmail().
+  withMessage("Email no valido!"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("El password es obligatorio!"),
+  handleInputErrors,
+  login
+);
 
 export default router;
