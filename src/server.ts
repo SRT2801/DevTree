1fed5ba3e30460fd7023cbd5cd9c64d294a5dd5e
SRT DEV <sahirruiz4@gmail.com>
2025-05-02 14:55:21 -0500

    Reorganizar importaciones y establecer conexión a la base de datos en el servidor

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express"; 
+import express from "express";
+import "dotenv/config";
 import router from "./router";
+import { connectDB } from "./config/db";
+
 const app = express();
 
-app.use(express.json()); 
-app.use('/',router)
+connectDB(); 
 
+app.use(express.json());
+app.use("/", router);
 
-export default app; 
+export default app;
