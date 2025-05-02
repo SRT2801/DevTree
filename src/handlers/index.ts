08430b2477c8b4e6a387422cacd7d2a94b91eb95
SRT DEV <sahirruiz4@gmail.com>
2025-05-02 14:55:33 -0500

    Agregar controladores para la creación de cuentas y el inicio de sesión de usuarios

---
+++ src/handlers/index.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import slug from "slug";
+import User from "../models/User";
+import { comparePassword, hashPassword } from "../utils/auth";
+
+export const createAccount = async (req: Request, res: Response) => {
+ 
+
+ 
+
+  const { email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    const error = new Error("El ya el correo ya esta resgistrado!");
+    res.status(409).json({ error: error.message });
+    return;
+  }
+
+  const handle = slug(req.body.handle, "");
+
+  const handleExists = await User.findOne({ handle });
+  if (handleExists) {
+    const error = new Error("Nombre de usuario no  disponible!");
+    res.status(409).json({ error: error.message });
+    return;
+  }
+
+  const user = new User(req.body);
+
+  user.password = await hashPassword(password);
+  user.handle = handle;
+
+  await user.save();
+
+  res.status(201).send("User created successfully!");
+};
+
+export const login = async (req: Request, res: Response) => {
+ 
+  // manejar errores
+
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  }
+ 
+  // revisar si el usuario existe
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("El Usuario no existe!");
+    res.status(404).json({ error: error.message });
+    return;
+  }
+
+  // comprobar el password
+  const isPasswordCorrect = await comparePassword(password, user.password)
+ 
+  if (!isPasswordCorrect) {
+    const error = new Error("Password incorrecto!");
+    res.status(401).json({ error: error.message });
+    return;
+  }
+
+  res.send("Login successful!");
+  
+};
